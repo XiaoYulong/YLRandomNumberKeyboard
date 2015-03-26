00fbd251bdd312e27ad4a47e261421fb5e07e8e5
XiaoYulong <xiaoyulong007@gmail.com>
2015-03-26 13:27:22 +0800

    add the keyboard

---
+++ YLRandomNumberKeyboard/YLRandomNumberKeyboard.h
@@ -0,0 +1,13 @@
+//
+//  YLRandomNumberKeyboard.h
+//  YLRandomNumberKeyboardDemo
+//
+//  Created by 肖 玉龙 on 15/3/26.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLRandomNumberKeyboard : UIView
+
+@end
---
+++ YLRandomNumberKeyboard/YLRandomNumberKeyboard.m
@@ -0,0 +1,21 @@
+//
+//  YLRandomNumberKeyboard.m
+//  YLRandomNumberKeyboardDemo
+//
+//  Created by 肖 玉龙 on 15/3/26.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import "YLRandomNumberKeyboard.h"
+
+@implementation YLRandomNumberKeyboard
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
