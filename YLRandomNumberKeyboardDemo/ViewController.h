ac7a638c9d103ecafe9f23c28137c71501676d95
XiaoYulong <xiaoyulong007@gmail.com>
2015-03-26 13:08:12 +0800

    Initial Commit

---
+++ YLRandomNumberKeyboardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLRandomNumberKeyboardDemo
+//
+//  Created by 肖 玉龙 on 15/3/26.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YLRandomNumberKeyboardDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YLRandomNumberKeyboardDemo
+//
+//  Created by 肖 玉龙 on 15/3/26.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
